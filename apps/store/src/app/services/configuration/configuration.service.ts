a61c673e184cce31f869573f6294a360e5943f7b
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-19 13:30:08 +0400

    Add Configuration service

--- apps/store/src/app/services/configuration/configuration.service.ts
+++ apps/store/src/app/services/configuration/configuration.service.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Configuration } from '@planout-store/api-interfaces';
 import { BehaviorSubject } from 'rxjs';
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +18,42 @@ export class ConfigurationService {
     productHeroImage: 'right',
     productThumbnailImage: 'bottom'
   });
+
+  public constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {
+
+    this.getUserIdAndCallGetConfiguration();
+
+  }
+
+  private getConfiguration(userId: number): void {
+
+    this.httpClient.get('http://localhost:3333/api/configuration/' + userId).subscribe(
+
+      (configuration: Configuration): void => {
+
+        this.configuration$.next(configuration);
+
+      }
+
+    );
+
+  }
+
+  private getUserIdAndCallGetConfiguration(): void {
+
+    this.userService.userId$.subscribe(
+
+      (userId: number): void => {
+
+        this.getConfiguration(userId);
+
+      }
+
+    );
+
+  }
 
 }
--- apps/store/src/app/services/user/user.service.ts
+++ apps/store/src/app/services/user/user.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService { }
+export class UserService {
+
+  public userId$: BehaviorSubject<number> = new BehaviorSubject(this.getRandomIntInclusive(1, 10));
+
+  private getRandomIntInclusive(min: number, max: number) {
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+
+  }
+
+}
